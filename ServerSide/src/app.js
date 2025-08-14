c9721f7f2059f789c860de8cb624ff72b6b89742
theinvincible180 <pandeyaniket701@gmail.com>
2025-08-14 14:24:47 +0530

    Initialized app.js

--- ServerSide/src/app.js
+++ ServerSide/src/app.js
@@ -1,2 +1,25 @@
 import express from "express"
 import cors from "cors"
+import cookieParser from "cookie-parser"
+import bodyParser from "body-parser"
+import dotenv from "dotenv"
+import morgan from "morgan";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.Client_URL,
+   
+    credentials:true
+}));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(morgan("dev"));
+
+//where dev is formate from 3 formates(dev , tiny , combined) 
+
+export default app;
