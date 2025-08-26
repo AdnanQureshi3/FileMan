5a36e0f988eb4d8f17c7a8c703665fb851997754
Adnan Qureshi <adnanquresh262@gmail.com>
2025-08-26 22:20:25 +0530

    aws leakage cured

--- client/src/components/UserDashBoard.jsx
+++ client/src/components/UserDashBoard.jsx
@@ -51,7 +51,7 @@ const UserDashBoard = () => {
            <>
           {/* <Home /> */}
           <StatsGrid />
-          {/* <FileShow /> */}
+          <FileShow />
          </>
            }
            
