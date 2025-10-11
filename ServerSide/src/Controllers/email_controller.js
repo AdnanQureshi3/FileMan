d32b76d4ca5230a290b0073117f2b0ce72b0123c
Adnan Qureshi <adnanquresh262@gmail.com>
2025-10-11 14:41:32 +0530

    testing

--- ServerSide/src/Controllers/email_controller.js
+++ ServerSide/src/Controllers/email_controller.js
@@ -1,9 +1,7 @@
 import nodemailer from "nodemailer";
 import User from "../models/user_Model.js";
-     const transporter = nodemailer.createTransport({ 
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+const transporter = nodemailer.createTransport({
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
