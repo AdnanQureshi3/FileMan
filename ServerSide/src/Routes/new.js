0d6a81897a87b4e62d330325419b803b1a70f1f3
Adnan Qureshi <adnanquresh262@gmail.com>
2025-10-23 17:51:58 +0530

    Prisma integrated

---
+++ ServerSide/src/Routes/new.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import { PrismaClient } from '../../../generated/prisma/index.js';
+
+
+const router = express.Router();
+
+const prisma = new PrismaClient();
+
+router.post('/new-route', async (req, res) => {
+    console.log("New route accessed");
+    try{
+        const { email, name } = req.body;
+      
+
+        const user = await prisma.user.create({
+            data: {
+                email,
+                name
+            }
+        });
+        console.log(user);      
+
+        res.json(user);
+    }
+    catch(err){
+        res.status(500).json({ error: err.message });
+    }
+});
+
+router.get('/show', async (req, res) => {
+    console.log("Show route accessed");
+    try{
+        const users = await prisma.user.deleteMany();
+        res.json(users); 
+    }
+    catch(err){
+        res.status(500).json({ error: err.message });
+    }
+});
+
+
+export default router;
